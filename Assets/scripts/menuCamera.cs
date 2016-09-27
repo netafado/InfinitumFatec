56d54f325d8c78a00b7bf89a9caf75506dc474ae
netafado <isaias.fran@gmail.com>
2016-09-27 13:51:40 -0300

    UI
    Conteudo relativo a tela inicial do jogo

---
+++ Assets/scripts/menuCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuCamera : MonoBehaviour {
+
+    private float limitX = 10000;
+    private float limitZ = 10000;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0.001f, 0, 0.001f);
+        transform.Rotate(0, 0.01f, 0);
+	}
+}
